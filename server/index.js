e897597a53c34cdc3007bc30208918e2fc108cf2
venemousCoder <ahmadgameriv@gmail.com>
2024-07-16 16:48:13 +0100

    some minor changes

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
-let express = require("express")
-let cors = require("cors")
-const mongoose = require("mongoose")
-let app = express()
-require("dotenv").config()
-let PORT = process.env.PORT || 4000
+let express = require("express");
+let cors = require("cors");
+const mongoose = require("mongoose");
+let app = express();
+require("dotenv").config();
+let PORT = process.env.PORT || 4000;
 
 //app configuration
 
 
 mongoose.connect(process.env.DB || "mongodb://localhost:27017/sms")
  .then(() => {
-    console.log("Connected to DB")
+    console.log("Connected to DB");
  }).catch((err) => {
-    throw new Error(err)
+    throw new Error(err);
  });
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors({
     origin: ["*"]
 }))
