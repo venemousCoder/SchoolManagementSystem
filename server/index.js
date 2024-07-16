fda823975e553af3668b128e599e3a9216d41d66
venemousCoder <ahmadgameriv@gmail.com>
2024-07-16 16:42:08 +0100

    Server and db config

---
+++ server/index.js
@@ -0,0 +1,23 @@
+let express = require("express")
+let cors = require("cors")
+const mongoose = require("mongoose")
+let app = express()
+require("dotenv").config()
+let PORT = process.env.PORT || 4000
+
+//app configuration
+
+
+mongoose.connect(process.env.DB || "mongodb://localhost:27017/sms")
+ .then(() => {
+    console.log("Connected to DB")
+ }).catch((err) => {
+    throw new Error(err)
+ });
+
+app.use(cors({
+    origin: ["*"]
+}))
+app.listen(PORT, () => {
+    console.log(`app running at PORT: ${PORT}`);
+})
