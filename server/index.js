ec1883ba601b8aa60cc8d9c8ce00deace6a92a14
venemousCoder <ahmadgameriv@gmail.com>
2024-07-16 16:58:44 +0100

    added some files

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 let app = express();
 require("dotenv").config();
 let PORT = process.env.PORT || 4000;
+const session = require('express-session');
 
 //app configuration
 
@@ -15,11 +16,19 @@ mongoose.connect(process.env.DB || "mongodb://localhost:27017/sms")
     throw new Error(err);
  });
 
+app.use(session({
+    cookie: { maxAge: 60000},
+    secret: process.env.SECRET_KEY,
+    saveUninitialized: false,
+    resave: false
+}))
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
     origin: ["*"]
 }))
+
 app.listen(PORT, () => {
     console.log(`app running at PORT: ${PORT}`);
 })
